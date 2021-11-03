285117045af93fd77f144d8f0d4587cce1de36f4
wsherry <sherryguiwang@gmail.com>
2021-11-03 00:16:44 -0400

    renamed and reorganized files

--- Python code/Post-processing and graphing/single_processing_example.py
+++ Python code/Post-processing and graphing/single_processing_example.py
@@ -17,9 +17,6 @@ all_key1 = df.challenge_key_1.to_list()
 all_key2 = df.challenge_key_2.to_list()
 is_point = df.is_point.to_list()
 
-fields = ['#', 'is_p','no_bit_flip_percentage', 'no_phase_flip_percentage', 'no_error_percentage']
-stats = pd.DataFrame(columns=fields)
-
 first_steane_codewords = ['0000000','1010101','0110011','1100110','0001111','1011010','0111100','1101001']
 second_steane_codewords = ['0000000', '1110000', '1001100', '0111100', '0101010', '1011010', '1100110', '0010110', '1101001', '0011001', '0100101', '1010101', '1000011', '0110011', '0001111', '1111111']
 # the codewords of our Steane encoded program
@@ -34,7 +31,13 @@ zkey = key2[1] + [0]*6
 del_x_key, del_z_key = get_delegated_OTP_keys(key1, xkey, zkey)
 processed_counts = apply_OTP_and_unpermute(counts, key1, del_x_key, del_z_key)
 bit, phase, all = check_correctness(processed_counts,codeword_combos)
-stats.loc[index] = [index, is_point[index], bit, phase, all]
 
-print(stats)
+print(index, is_point[index], bit, phase, all)
+
+# uncomment for adding error rates to a dataframe
+# fields = ['#', 'is_p','no_bit_flip_percentage', 'no_phase_flip_percentage', 'no_error_percentage']
+# stats = pd.DataFrame(columns=fields)
+# stats.loc[index] = [index, is_point[index], bit, phase, all]
+# print(stats)
+
 
